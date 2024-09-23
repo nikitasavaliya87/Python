6f1c7ad700f5ae35d2f0bba3bd1741d7915700b4
nikitasavaliya87 <144912665+nikitasavaliya87@users.noreply.github.com>
2024-09-23 19:22:48 +1000

    Fetching sunrise and sunset information from sunrise-sunset api

--- ISS (International Space Station Current Location)overhead notifier/main.py
+++ ISS (International Space Station Current Location)overhead notifier/main.py
@@ -1,5 +1,6 @@
 import requests
 from tkinter import *
+from datetime import datetime
 # response=requests.get(url="http://api.open-notify.org/iss-now.json")
 # #print(response)
 # #print(response.status_code)
@@ -14,29 +15,50 @@ from tkinter import *
 
 
 
-def get_quote():
-    response=requests.get(url="https://api.kanye.rest/")
-    #print(response)
-    data=response.json()["quote"]
-    #quote_text = canvas.create_text(150, 207, text=data, width=250, font=("Arial", 30, "bold"), fill="white")
-    #print(data)
-    canvas.itemconfig(quote_text,text=data)
+# def get_quote():
+#     response=requests.get(url="https://api.kanye.rest/")
+#     #print(response)
+#     data=response.json()["quote"]
+#     #quote_text = canvas.create_text(150, 207, text=data, width=250, font=("Arial", 30, "bold"), fill="white")
+#     #print(data)
+#     canvas.itemconfig(quote_text,text=data)
 
 
-window = Tk()
-window.title("Kanye Says...")
-window.config(padx=50, pady=50)
+# window = Tk()
+# window.title("Kanye Says...")
+# window.config(padx=50, pady=50)
 
-canvas = Canvas(width=300, height=414)
-background_img = PhotoImage(file="ISS (International Space Station Current Location)overhead notifier/images/background.png")
-canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
-canvas.grid(row=0, column=0)
+# canvas = Canvas(width=300, height=414)
+# background_img = PhotoImage(file="ISS (International Space Station Current Location)overhead notifier/images/background.png")
+# canvas.create_image(150, 207, image=background_img)
+# quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
+# canvas.grid(row=0, column=0)
 
-kanye_img = PhotoImage(file="ISS (International Space Station Current Location)overhead notifier/images/kanye.png")
-kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
-kanye_button.grid(row=1, column=0)
+# kanye_img = PhotoImage(file="ISS (International Space Station Current Location)overhead notifier/images/kanye.png")
+# kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+# kanye_button.grid(row=1, column=0)
 
 
 
-window.mainloop()
+# window.mainloop()
+latitude=-38.116530
+longitude=145.326610
+
+position={
+"lat":latitude,
+"lng":longitude,
+"formatted":0
+
+}
+response=requests.get(" https://api.sunrise-sunset.org/json",params=position)
+response.raise_for_status()
+data=response.json()
+sunrise=data["results"]["sunset"].split("T")[1].split(":")[0]
+sunset=data["results"]["sunrise"].split("T")[1].split(":")[0]
+print(sunrise)
+print(sunset)
+
+#print(data)
+
+now=datetime.now()
+print(now.hour)
