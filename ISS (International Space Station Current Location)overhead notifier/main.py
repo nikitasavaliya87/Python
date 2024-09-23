6dbb626384488c287b86e6b83b8a89337067994b
nikitasavaliya87 <144912665+nikitasavaliya87@users.noreply.github.com>
2024-09-23 19:55:44 +1000

    Fetch API data project completed

--- ISS (International Space Station Current Location)overhead notifier/main.py
+++ ISS (International Space Station Current Location)overhead notifier/main.py
@@ -1,6 +1,8 @@
 import requests
 from tkinter import *
 from datetime import datetime
+import time
+import smtplib
 # response=requests.get(url="http://api.open-notify.org/iss-now.json")
 # #print(response)
 # #print(response.status_code)
@@ -41,24 +43,73 @@ from datetime import datetime
 
 
 # window.mainloop()
+# latitude=-38.116530
+# longitude=145.326610
+
+# position={
+# "lat":latitude,
+# "lng":longitude,
+# "formatted":0
+
+# }
+# response=requests.get(" https://api.sunrise-sunset.org/json",params=position)  # passing parameters
+# response.raise_for_status()
+# data=response.json()
+# sunrise=data["results"]["sunset"].split("T")[1].split(":")[0]
+# sunset=data["results"]["sunrise"].split("T")[1].split(":")[0]
+# print(sunrise)
+# print(sunset)
+
+# #print(data)
+
+# now=datetime.now()
+# print(now.hour)
+
+
 latitude=-38.116530
 longitude=145.326610
 
-position={
-"lat":latitude,
-"lng":longitude,
-"formatted":0
+my_email="n270920161987@gmail.com"
+password="zqij qhyw vxjv gaop"
 
-}
-response=requests.get(" https://api.sunrise-sunset.org/json",params=position)
-response.raise_for_status()
-data=response.json()
-sunrise=data["results"]["sunset"].split("T")[1].split(":")[0]
-sunset=data["results"]["sunrise"].split("T")[1].split(":")[0]
-print(sunrise)
-print(sunset)
 
-#print(data)
+def is_iss_overhead():
+    response=requests.get(url="http://api.open-notify.org/iss-now.json")
 
-now=datetime.now()
-print(now.hour)
+    response.raise_for_status()
+    data=response.json()
+    print(data)
+
+    iss_lat=float(data["iss_position"]["latitude"])
+    iss_lug=float(data["iss_position"]["latitude"])
+
+    if latitude-5 <= iss_lat<= latitude+5 and longitude-5 <= iss_lug <= longitude+5:
+        return True
+
+def is_night():
+    position={
+        "lat":latitude,
+        "lng":longitude,
+        "formatted":0
+
+        }
+    response=requests.get(" https://api.sunrise-sunset.org/json",params=position)  # passing parameters
+    response.raise_for_status()
+    data=response.json()
+    sunrise=int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    sunset=int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    now=datetime.now().hour
+
+    if now >=sunset or now <=sunrise:
+        return True
+while True:    
+    time.sleep(60)
+    if is_iss_overhead() and is_night():
+        connection =smtplib.SMTP("smtp.gmail.com") 
+        connection.starttls()
+        connection.login(user=my_email,password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=my_email,
+            msg="Subject:Look Up\n\n The ISS is above in the sky. ")
+        #connection.close()
