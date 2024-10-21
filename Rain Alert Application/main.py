f51f7fb8018fcf6d0b78f4c620114fa539bbe33a
nikitasavaliya87 <144912665+nikitasavaliya87@users.noreply.github.com>
2024-10-21 14:01:22 +1100

    fetch data with authentication in API

--- Rain Alert Application/main.py
+++ Rain Alert Application/main.py
@@ -1,2 +1,16 @@
 
-print("ApI Application")
+#
+# print("ApI Application")
+import requests
+
+end="https://api.openweathermap.org/data/2.5/forecast"
+api_key="083334a04b5674ede874e51dd8055ccf"
+weather_param={
+"lat":-37.813629,
+"lon":144.963058,
+"appid":api_key,
+
+}
+response=requests.get(end,params=weather_param)
+#print(response.status_code)
+print(response.json())
