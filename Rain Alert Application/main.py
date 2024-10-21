4308164571d847cbba39b3b43411b806bdfec94a
nikitasavaliya87 <144912665+nikitasavaliya87@users.noreply.github.com>
2024-10-21 14:09:29 +1100

    Updating

--- Rain Alert Application/main.py
+++ Rain Alert Application/main.py
@@ -8,9 +8,12 @@ api_key="083334a04b5674ede874e51dd8055ccf"
 weather_param={
 "lat":-37.813629,
 "lon":144.963058,
+"cnt":4,
 "appid":api_key,
 
 }
 response=requests.get(end,params=weather_param)
 #print(response.status_code)
-print(response.json())
+response.raise_for_status()
+weather_data=response.json()
+print(weather_data)
