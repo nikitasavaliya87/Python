3458247259dc548210f8780f91df463deea8469d
nikitasavaliya87 <144912665+nikitasavaliya87@users.noreply.github.com>
2024-11-11 14:02:47 +1100

    updating

--- Cheap Flight Finder/data_manager.py
+++ Cheap Flight Finder/data_manager.py
@@ -1,6 +1,34 @@
 import os
 import requests
+from requests.auth import HTTPBasicAuth
+from dotenv import load_dotenv
 
+load_dotenv(override=True)
+sheety_endpoint=os.getenv("SHEETY_ENDPOINT")
 class DataManager:
+    def __init__(self):
+        self.user=os.getenv("S_USERNAME")
+        self.password=os.getenv("S_PASSWORD")
+        self.authorization=HTTPBasicAuth(self.user, self.password)
+        self.destination_data={}
+
+    def get_destination_data(self):
+        response=requests.get(url=sheety_endpoint, auth=self.authorization)
+        data=response.json()
+        self.destination_data=data["prices"]
+        print(data)
+        return self.destination_data
     
-    pass
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{sheety_endpoint}/{city['id']}",
+                json=new_data,
+                auth=self.authorization
+            )
+            print(response.text)
--- Cheap Flight Finder/flight_search.py
+++ Cheap Flight Finder/flight_search.py
@@ -1,3 +1,7 @@
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
+    
+    
+    def get_destination_code(self, city_name):
+        code="TESTING"
+        return code
     pass
